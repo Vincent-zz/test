73787c0c00a5b3a4363a69623b46de7fbc86e6ed
Vincent Zheng <donvincent@sjtu.edu.cn>
2021-12-05 21:02:29 +0800

    classical

--- classical beginning.cpp
+++ classical beginning.cpp
@@ -2,12 +2,5 @@
 using namespace std;
 int main(){
     cout << "Hello world!" << endl;
-    int *p = new int [10];
-    *(p + 1) = 666;
-    delete [] p;
-    char *s[6];
-    s[0] = new char [10];
-    delete [] s[0];
-    cout << "Hello world again!" << endl;
     return 0;
 }
